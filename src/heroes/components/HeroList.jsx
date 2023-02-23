4527c13185184cbc4ec0385c4fcdbd962b080a09
Byron <byrons-93@hotmail.com>
2023-02-23 17:11:01 -0500

    add hero card and hero list

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { getHeroesByPublisher } from '../helpers'
+import { HerCard } from './HerCard'
 
 export const HeroList = ({publisher}) => {
     const heroes =getHeroesByPublisher(publisher)
   return (
-    <ul>
-        {
-            heroes.map (hero => (
-                <li key={hero.id}>
-                    {hero.superhero}
-                </li>
+      <div className='row row-cols-1 row-cols-md-3 g-3'>
+      {
+          heroes.map (hero => (
+                <HerCard 
+                key={hero.id}
+                {...hero}
+                />
             ))
         }
-    </ul>
+    </div>
   )
 }
